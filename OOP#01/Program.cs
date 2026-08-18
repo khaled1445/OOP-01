5dfe40dbfbc7e701fb34a8a21b672206cbd04c6f
5aled998 <khaledamr142005@.com>
2026-08-18 11:44:07 +0300

    Question01

--- OOP#01/Program.cs
+++ OOP#01/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //OOP 01 - Smart Delivery Management System 
+            #region Part 01 : Theoretical Questions
+
+            #region Question 1 
+            //a- DeliveryAddress variable:
+            //first DeliveryAddress is a struct (value type) so when you take a copy in another value it makes
+            //a new seprate stackframe in the stack put in the new data and the old variable doesn't affected
+
+            //b- Customer variable:
+            // Customer is a class (Refrence Type) so when you take a copy you just take the refrence in the stack and
+            // it leads you to the same object in Heap so you'll OverWrite the old variable so the two variables will have
+            // the same refreance so tthe old one will change to whe new var
+
+            #endregion
+
+            #endregion
         }
+
     }
+    
 }
